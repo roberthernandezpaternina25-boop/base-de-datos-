9d89053c5a2d5192893ba9b1ab9eaa917210f46d
rober hernandez paternina <roberthernandezpaternina25@gmail.com>
2026-08-09 09:34:25 -0500

    datos

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require("express");
+import express from "express";
+import { config } from "dotenv";
+import pg from "pg";
+
+config();
 
 const app = express();
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true 
+});
 
-app.get("/", (req, res) => {
-  res.send("eres mi cata, como dice la niña jajajajaja!");
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT now()");
+  res.send(`eres mi cata, como dice la niña jajajajaja! ${result.rows[0].now}`);
+});
+
+app.get('/ping', async (req, res) => {
+  const result = await pool.query("SELECT now()")
+  return res.json(result.rows[0])
 });
 
 app.listen(3000, () => {
